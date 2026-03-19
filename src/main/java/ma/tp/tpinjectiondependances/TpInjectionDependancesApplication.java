8c44979589ae0bee9b36895b1c4cb099f6f201ba
zouhair <zo_abouhfas@etu.enset-media.ac.ma>
2026-03-19 04:18:47 +0100

    Initial commit

---
+++ src/main/java/ma/tp/tpinjectiondependances/TpInjectionDependancesApplication.java
@@ -0,0 +1,13 @@
+package ma.tp.tpinjectiondependances;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpInjectionDependancesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpInjectionDependancesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ma/tp/tpinjectiondependances/TpInjectionDependancesApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.tp.tpinjectiondependances;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpInjectionDependancesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
